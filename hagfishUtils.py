1756447abd43c4962672bd95c9222a235c373f2c
Mark Fiers <mark.fiers.42@gmail.com>
2010-07-16 11:41:53 +1200

    ongoing work

--- hagfishUtils.py
+++ hagfishUtils.py
@@ -1,5 +1,6 @@
 import os
 import math
+import copy
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -20,14 +21,10 @@ COLOR3 = '#35488B' #purple
 COLOR4 = '#DFC92C' #yellow
 COLOR5 = '#2AB5AA' #cyan
 
-
 COLMAP1 = mpl.colors.LinearSegmentedColormap.from_list(
     'COLMAP1', 
-    [COLOR2, COLOR4], N=100)
+    [COLOR2, COLOR3, COLOR1], N=200)
 
-COLMAP2 = mpl.colors.LinearSegmentedColormap.from_list(
-    'COLMAP2', 
-    [COLOR3, COLOR5], N=100)
 
 ################################################################################
 ## set up logging
@@ -69,8 +66,6 @@ def addPlotParameters(parser):
     parser.add_option('-i', dest='inputFile',
                       help='input file with the coverage data (npz, if not specified, '+
                       'the input file name will be inferred from the sequence Id')
-    parser.add_option('-l', dest='log', action='store_true',
-                      help='take the log10 of the coverage')
 
     parser.set_defaults(imageWidth=1000)
     parser.set_defaults(bandHeight=200)
@@ -139,7 +134,12 @@ class hagfishData:
         self.vectors.append('okh')
         self.medianH = np.median(self.okh)
         self.median = np.median(self.ok)
-
+        self.average = np.average(self.ok)
+        self.min = np.min(self.ok)
+        self.max = np.max(self.ok)
+        self.l.info("stats: median ok is %s (h %s)" % (self.median, self.medianH))
+        self.l.info("stats: average ok is %s" % (self.average))
+        self.l.info("stats: min, max ok is %s, %s" % (self.min, self.max))
         #some stuff for plotting
         self.x = np.arange(0, self.seqLen, dtype="int")
         #self.z = np.zeros_like(self.x)
@@ -147,7 +147,7 @@ class hagfishData:
 
 class hagfishPlot:
 
-    def __init__(self, options, data):
+    def __init__(self, options, data, title=None):
         self.l = getLogger('plot', options.verbose)
         self.options = options
         self.data = data
@@ -194,7 +194,7 @@ class hagfishPlot:
 
         if options.ymax:
             self.maxY = int(options.ymax)
-            self.minY = -maxY
+            self.minY = -self.maxY
         else:
             self.maxY = int(quant(self.data.okh, options.yfrac))
             self.minY = -self.maxY
@@ -205,27 +205,34 @@ class hagfishPlot:
         Prepare matplotlib
         """
         mpl.rcParams['axes.labelsize'] = 'x-small'
+        mpl.rcParams['axes.titlesize'] = 'small'
         mpl.rcParams['xtick.labelsize'] = 'xx-small'
         mpl.rcParams['ytick.labelsize'] = 'xx-small'
 
         self.figWidth = self.options.imageWidth / 100.0
-        self.bandHeight = self.options.bandHeight / 100.0
-        self.figHeight = self.bandHeight * self.noBands
+        self.bandHeight = float(self.options.bandHeight) / 100.0
+        tbh = self.bandHeight * self.noBands
+        self.figHeight = tbh + 1
         self.fig = plt.figure(figsize=(self.figWidth, self.figHeight))
-        figtb = 0.2 / self.noBands
-        self.l.debug("no bands: %d" % self.noBands)
-        self.axCoords = 0.05, figtb, 0.9, 1 - (2*figtb)
+        figtb = 1.0 / (3 * (tbh+1))
+        self.l.debug("no bands: %d " % self.noBands)
+        self.l.debug("top/bottom margin: %f (0.5 / %f ) " % (figtb, tbh))
+        #lbrt
+        self.axCoords = 0.08, figtb, 0.85, 1 - (2*figtb)
         self.l.debug("creating axes with coords lbrt %.4f %.4f %.4f %.4f " %
                      self.axCoords)
 
         self.ax = self.fig.add_axes(self.axCoords)
 
-        if self.start == 0:
-            self.ax.set_title('Coverage plot for "%s" (0 to %.0e)' % (
-                self.data.seqId, self.stop))
+        if not title:
+            if self.start == 0:
+                self.ax.set_title('Coverage plot for "%s" (0 to %.2e)' % (
+                    self.data.seqId, self.stop))
+            else:
+                self.ax.set_title('Coverage plot for "%s" (%.2e to %.2e)' % (
+                    self.data.seqId, self.start, self.stop))
         else:
-            self.ax.set_title('Coverage plot for "%s" (%.0e to %.0e)' % (
-                self.data.seqId, self.start, self.stop))
+            self.ax.set_title(title)
 
     def plotBands(self, bandPlotter):
         yCorrect = 0
@@ -234,9 +241,10 @@ class hagfishPlot:
             b = bandPlotter(self, band, self.options)
             yCorrect -= self.YCorrPerBand
             b.setYcorrection(yCorrect)
+            self.l.debug("start plotting band %d (corr %s)" % (band, yCorrect))
 
             self.yTicks.append(yCorrect)
-            self.yTickLabels.append("%.0e" % b.start)
+            self.yTickLabels.append("%.2e" % b.start)
 
             b.setYticks2()
             if band == 0:
@@ -257,17 +265,21 @@ class hagfishPlot:
         y2.get_yaxis().set_ticklabels(self.yTickLabels2)
         y2.set_ylim(self.tminY,self.tmaxY)
 
-    def save(self):
+    def save(self, tag=""):
         self.ax.set_xlim(0, self.ntPerBand)
         if self.options.outfile:            
             outFileName = self.options.outfile
         else:
             outFileName = self.data.seqId
 
+        if tag:
+            outFileName += '_%s' % tag
+            
         if self.options.start or self.options.stop:
             outFileName += "_%d_%d" % (self.start, self.stop)
 
         for f in self.options.format:
+            self.l.info("writing to %s.%s" % (outFileName, f))
             plt.savefig('%s.%s' % (outFileName, f), dpi=100)
 
 
@@ -283,10 +295,9 @@ class hagfishPlotBand:
         self.band = band
         self.start = self.plot.start + (band * self.plot.ntPerBand)
         self.stop = self.start + (self.plot.ntPerBand - 1)
-        self.l.debug(
-            'printing from %d to %d' % (
-                self.start, self.stop))
+        self.l.debug( 'printing from %d to %d' % ( self.start, self.stop))
 
+        self.l.debug("preprocessing band data")
         for v in self.data.vectors:
             self.__dict__[v] = self.data.__dict__[v][self.start:self.stop]
             
@@ -316,16 +327,7 @@ class hagfishPlotBand:
                        (0.5 * self.plot.YCorrPerBand)
         self.bandBottom = self.yCorrection - \
                        (0.5 * self.plot.YCorrPerBand)
-        self.l.debug("defining band stats")
-        self.l.debug("   ycorrection %s" % self.yCorrection)
-        self.l.debug("   bandTop     %s" % self.bandTop)
-        self.l.debug("   bandBottom  %s" % self.bandBottom)
-        #TransformedBbox(rect[0].get_bbox(), ax.transData) 
-        #self.bbox = matplotlib.transforms.TransformedBbox(
-        #     matplotlib.transforms.Bbox(
-        #        np.array([[0,self.bandBottom],
-        #                  [self.plot.ntPerBand,self.bandTop]])),
-        #    self.ax.transData)
+        self.l.debug("defining band from %s to %s" % (self.bandTop, self.bandBottom))
         _pd = [
             (mpath.Path.MOVETO, (0, self.bandTop)),
             (mpath.Path.LINETO, (self.plot.ntPerBand, self.bandTop)),
