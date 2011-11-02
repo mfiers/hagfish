ec5946bd82115d1cdd6b183516bbc69779c2f1f6
Mark Fiers <mark.fiers@plantandfood.co.nz>
2011-11-02 16:28:11 +1300

    it appears that not all gzip objects like the "with" statement

--- hagfish_file_util.py
+++ hagfish_file_util.py
@@ -7,15 +7,17 @@ def np_savez(file_base, **kwargs):
     """
     for k in kwargs:
         file_name = '%s.%s.gz' % (file_base, k)
-        with gzip.open(file_name, 'w') as F:
-            cPickle.dump(kwargs[k], F)
+        F = gzip.open(file_name, 'w')
+        cPickle.dump(kwargs[k], F)
+        F.close()
 
 def np_load(file_base, what):
     """
     reimplement the crappy np.savez function
     """
     file_name = '%s.%s.gz' % (file_base, what)
-    with gzip.open(file_name, 'r') as F:
-        data = cPickle.load(F)
+    F = gzip.open(file_name, 'r')
+    data = cPickle.load(F)
+    F.close()
     return data
 
